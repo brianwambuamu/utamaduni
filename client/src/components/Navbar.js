78b1b47460a6c49f82e9ec4b7a0ffc92319a92fd
Brian Wambua <mutisobrian210@gmail.com>
2025-06-15 00:09:45 +0300

    Home

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -60,6 +60,16 @@ function Navbar() {
                 Products
               </Link>
             </li>
+             <li className='nav-item'>
+              <Link
+                to='/contact-us'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Contact Us
+              </Link>
+            </li>
+
 
             <li>
               <Link
